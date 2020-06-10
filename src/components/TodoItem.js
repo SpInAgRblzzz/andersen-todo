044a787c4762d6a61454b4d8fe208ce023db3193
Kirill Kruchenok <spinagrblzzz@gmail.com>
2020-06-10 14:28:27 +0300

    refactor: comments deleted

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -17,7 +17,6 @@ function TodoItem({
 	const [inputValue, setInputValue] = useState(todoValue);
 	const [subTodoAdded, setSubTodoAdded] = useState(false);
 	const [correctionMode, setCorrectionMode] = useState(false);
-	//const [checkStatus, setCheck] = useState(isChecked);
 
 	function submitChanges(e) {
 		e.preventDefault();
@@ -27,15 +26,6 @@ function TodoItem({
 
 	function toggleCheckStatus() {
 		toggleCheck(isChecked, idRoute);
-		/* setTodos(
-			todos.map((item) => {
-				if (item.id === id) {
-					item.isChecked = !checkStatus;
-				}
-				return item;
-			})
-		);
-		setCheck(!checkStatus); */
 	}
 
 	const handleInput = (e) => setInputValue(e.target.value);
